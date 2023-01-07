36709eb80d3c53d8f16068182427c10e2fb73ca6
Bludidi <bludidi5@gmail.com>
2023-01-07 11:35:40 +0200

    add style in contact page

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -5,7 +5,50 @@ const Contact = () => (
   <>
     <div className="logo-img" />
     <div className="mid-section contact">
-      <h3>Welcome to contact me!</h3>
+      <div className="contact-div">
+        <h2 className="contactme">CONTACT ME</h2>
+        <p className="contact-info">
+          If you have an application you are interested in developing,
+          <br />
+          a feature that you need built or a project that needs coding,
+          <br />
+          simple slide a message below.
+        </p>
+        <form
+          action="https://formspree.io/f/form"
+          method="post"
+          id="form"
+          className="contact-form form-item"
+        >
+          <input
+            className="contact_input one_line"
+            type="text"
+            placeholder="username"
+            name="username"
+            maxLength="30"
+            required
+          />
+
+          <input
+            className="contact_input one_line"
+            type="email"
+            placeholder="Enter email"
+            name="email"
+            required
+          />
+
+          <textarea
+            className="message contact_input"
+            placeholder="Write your message here"
+            maxLength="500"
+            required
+          />
+
+          <button className="get-intouch btn1" type="submit">
+            Get in touch
+          </button>
+        </form>
+      </div>
     </div>
   </>
 );
