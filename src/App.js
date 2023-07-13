992aed88fb01b49d0e1d36b2c287c0abf81b05f5
Bludidi <bludidi5@gmail.com>
2023-07-13 11:47:11 +0200

    Update smooth scroll

--- src/App.js
+++ src/App.js
@@ -1,8 +1,5 @@
-/* eslint-disable */
 import React from 'react';
-import { Routes, Route } from 'react-router-dom';
 import Navbar from './components/Navbar';
-// import Footer from './components/footer'
 import Home from './components/Home';
 import About from './components/About';
 import Contact from './components/Contact';
@@ -10,18 +7,24 @@ import Projects from './components/Projects';
 
 const App = () => (
   <>
-      <div className="app">
-        <Navbar />
-        <div className="view-port">
-          <Routes className="view-port">
-            <Route path="/" element={<Home />} />
-            <Route path="/projects" element={<Projects />} />
-            <Route path="/about" element={<About />} />
-            <Route path="/contact" element={<Contact />} />
-          </Routes>
-        </div>
+    <div className="app">
+      <Navbar />
+      <div className="view-port">
+        <section id="home">
+          <Home />
+        </section>
+        <section id="projects">
+          <Projects />
+        </section>
+        <section id="about">
+          <About />
+        </section>
+        <section id="contact">
+          <Contact />
+        </section>
       </div>
+    </div>
   </>
-)
+);
 
 export default App;
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -5,58 +5,79 @@ import * as SiIcons from 'react-icons/si';
 import logo from '../images/logo/bludidi3.png';
 import './styles/Navbar.css';
 
-const Navbar = () => (
-  <nav className="navigation">
-    <div className="logo-div">
-      <img src={logo} alt="logo" className="logo" />
-    </div>
-    <NavLink
-      to="/"
-      exact
-      className="d-flex nav-text link"
-      activeClassName="active"
-    >
-      <AiIcons.AiFillHome size={30} />
-      <span className="nav-link-name">HOME</span>
-    </NavLink>
-    <NavLink
-      to="/projects"
-      className="d-flex nav-text link"
-      activeClassName="active"
-    >
-      <FaIcons.FaRegFolderOpen size={30} />
-      <span className="nav-link-name">PROJECTS</span>
-    </NavLink>
-    <NavLink
-      to="/About"
-      className="d-flex nav-text link"
-      activeClassName="active"
-    >
-      <FaIcons.FaInfoCircle size={30} />
-      <span className="nav-link-name">ABOUT</span>
-    </NavLink>
-    <NavLink
-      to="/Contact"
-      className="d-flex nav-text link"
-      activeClassName="active"
-    >
-      <AiIcons.AiOutlineMail size={30} />
-      <span className="nav-link-name">CONTACT</span>
-    </NavLink>
-    <br />
-    <div style={{ textAlign: 'center', color: '#fff' }}>
-      <hr />
-      <em>My Socials</em>
-      <hr />
-    </div>
-    {/* <br /> */}
-    <div className="my-socials">
-      <a href="https://github.com/Bludidi" aria-label="GitHub"><FaIcons.FaGithub size={25} className="my-social-icons" /></a>
-      <a href="https://twitter.com/@BB_Ludidi" aria-label="Twitter"><FaIcons.FaTwitter size={25} className="my-social-icons" /></a>
-      <a href="https://linkedin.com/in/brian-ludidi" aria-label="LinkedIn"><AiIcons.AiOutlineLinkedin size={25} className="my-social-icons" /></a>
-      <a href="mailto:bludidi5@gmail.com" aria-label="Email"><SiIcons.SiGmail size={25} className="my-social-icons" /></a>
-    </div>
-  </nav>
-);
+const Navbar = () => {
+  const handleSmoothScroll = (id) => {
+    const element = document.getElementById(id);
+    if (element) {
+      element.scrollIntoView({ behavior: 'smooth' });
+    }
+  };
+
+  return (
+    <nav className="navigation">
+      <div className="logo-div">
+        <img src={logo} alt="logo" className="logo" />
+      </div>
+      <NavLink
+        to="/"
+        exact
+        className="d-flex nav-text link"
+        activeClassName="active"
+        onClick={() => handleSmoothScroll('home')}
+      >
+        <AiIcons.AiFillHome size={30} />
+        <span className="nav-link-name">HOME</span>
+      </NavLink>
+      <NavLink
+        to="/projects"
+        className="d-flex nav-text link"
+        activeClassName="active"
+        onClick={() => handleSmoothScroll('projects')}
+      >
+        <FaIcons.FaRegFolderOpen size={30} />
+        <span className="nav-link-name">PROJECTS</span>
+      </NavLink>
+      <NavLink
+        to="/About"
+        className="d-flex nav-text link"
+        activeClassName="active"
+        onClick={() => handleSmoothScroll('about')}
+      >
+        <FaIcons.FaInfoCircle size={30} />
+        <span className="nav-link-name">ABOUT</span>
+      </NavLink>
+      <NavLink
+        to="/Contact"
+        className="d-flex nav-text link"
+        activeClassName="active"
+        onClick={() => handleSmoothScroll('contact')}
+      >
+        <AiIcons.AiOutlineMail size={30} />
+        <span className="nav-link-name">CONTACT</span>
+      </NavLink>
+      <br />
+      <div style={{ textAlign: 'center', color: '#fff' }}>
+        <hr />
+        <em>My Socials</em>
+        <hr />
+      </div>
+      {/* <br /> */}
+      <div className="my-socials">
+        <a href="https://github.com/Bludidi" aria-label="GitHub">
+          <FaIcons.FaGithub size={25} className="my-social-icons" />
+        </a>
+        <a href="https://twitter.com/@BB_Ludidi" aria-label="Twitter">
+          <FaIcons.FaTwitter size={25} className="my-social-icons" />
+        </a>
+        <a href="https://linkedin.com/in/brian-ludidi" aria-label="LinkedIn">
+          <AiIcons.AiOutlineLinkedin size={25} className="my-social-icons" />
+        </a>
+        <a href="mailto:bludidi5@gmail.com" aria-label="Email">
+          <SiIcons.SiGmail size={25} className="my-social-icons" />
+        </a>
+      </div>
+    </nav>
+  );
+};
 
 export default Navbar;
