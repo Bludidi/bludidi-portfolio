9ffeee0ebf09af0b95ea0a8c38ef9d717e2ae2d1
Bludidi <bludidi5@gmail.com>
2023-07-13 04:51:46 +0200

    Add images

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,12 +1,23 @@
 import React from 'react';
+import * as AiIcons from 'react-icons/ai';
 import '@fortawesome/fontawesome-free/css/all.css';
 import './styles/Home.css';
 import './styles/icons.css';
-import profilePic from '../images/profile-pic.jpg';
+import profilePic1 from '../images/profile-images/bl-profile-bw-nb.png';
+import profilePic2 from '../images/profile-images/bl-profile-color-nb.png';
 
 const Home = () => (
   <>
     <div className="home">
+      <div className="banner">
+        <AiIcons.AiOutlineUser size={40} className="head-icon" />
+        <h2>Welcome to Brian&apos;s Portfolio</h2>
+      </div>
+      <div className="line">
+        <hr />
+      </div>
+      <br />
+      <br />
       <div className="main main-home">
         <div className="home-text welcome">
           <p className="home-pgraph">
@@ -34,7 +45,8 @@ const Home = () => (
           </p>
         </div>
         <div className="pics">
-          <img src={profilePic} alt="profile" className="pic" />
+          <img src={profilePic1} alt="profile" className="pic" />
+          <img src={profilePic2} alt="profile" className="pic2" />
         </div>
       </div>
     </div>
