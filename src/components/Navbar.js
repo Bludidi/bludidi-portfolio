45dd6a49d7568e24e35d5b1e0739eb53e4197640
Bludidi <bludidi5@gmail.com>
2023-08-05 14:49:01 +0200

    Add blank tab

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -63,16 +63,16 @@ const Navbar = () => {
       </div>
       {/* <br /> */}
       <div className="my-socials">
-        <a href="https://github.com/Bludidi" aria-label="GitHub">
+        <a href="https://github.com/Bludidi" target="_blank" rel="noopener noreferrer" aria-label="GitHub">
           <FaIcons.FaGithub size={25} className="my-social-icons" />
         </a>
-        <a href="https://twitter.com/@BB_Ludidi" aria-label="Twitter">
+        <a href="https://twitter.com/@BB_Ludidi" target="_blank" rel="noopener noreferrer" aria-label="Twitter">
           <FaIcons.FaTwitter size={25} className="my-social-icons" />
         </a>
-        <a href="https://linkedin.com/in/brian-ludidi" aria-label="LinkedIn">
+        <a href="https://linkedin.com/in/brian-ludidi" target="_blank" rel="noopener noreferrer" aria-label="LinkedIn">
           <AiIcons.AiOutlineLinkedin size={25} className="my-social-icons" />
         </a>
-        <a href="mailto:bludidi5@gmail.com" aria-label="Email">
+        <a href="mailto:bludidi5@gmail.com" target="_blank" rel="noopener noreferrer" aria-label="Email">
           <SiIcons.SiGmail size={25} className="my-social-icons" />
         </a>
       </div>
--- src/components/icons.js
+++ src/components/icons.js
@@ -10,22 +10,22 @@ import './styles/icons.css';
 const Icons = () => (
   <>
     <div className="social-icons">
-      <a href="https://github.com/Bludidi" target="_blank" rel="noreferrer">
+      <a href="https://github.com/Bludidi" target="_blank" rel="noopener noreferrer">
         <img src={myImage1} alt="github icon" className="socials" />
       </a>
-      <a href="/Contact" rel="noreferrer">
+      <a href="/Contact" rel="noopener noreferrer">
         <img src={myImage2} alt="mail icon" className="socials" />
       </a>
-      <a href="https://twitter.com/BB_Ludidi" target="_blank" rel="noreferrer">
+      <a href="https://twitter.com/BB_Ludidi" target="_blank" rel="noopener noreferrer">
         <img src={myImage3} alt="twitter icon" className="socials" />
       </a>
-      <a href="https://www.linkedin.com/in/brian-ludidi" target="_blank" rel="noreferrer">
+      <a href="https://www.linkedin.com/in/brian-ludidi" target="_blank" rel="noopener noreferrer">
         <img src={myImage4} alt="linkedin icon" className="socials" />
       </a>
-      <a href="https://github.com" target="_blank" rel="noreferrer">
+      <a href="https://github.com" target="_blank" rel="noopener noreferrer">
         <img src={myImage5} alt="angellist icon" className="socials" />
       </a>
-      <a href="https://github.com" target="_blank" rel="noreferrer">
+      <a href="https://github.com" target="_blank" rel="noopener noreferrer">
         <img src={myImage6} alt="medium icon" className="socials" />
       </a>
     </div>
